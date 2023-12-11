22de812b5d62c57e5631f5972db99bb4eef65063
James Lovatt <james.lovatt@planetaryprocessing.io>
2023-12-11 12:35:19 +0000

    prevent editing associated lua with instances of entities

--- pp_entity_node.gd
+++ pp_entity_node.gd
@@ -6,4 +6,17 @@ static var is_entity_node: bool = true
 @export_category("Entity Properties")
 @export_multiline var data = ''
 @export var chunkloader = false
-@export_file("*.lua") var lua_path: String = ''
+var lua_path = ''
+
+func _get_property_list() -> Array[Dictionary]:
+	var properties: Array[Dictionary] = []
+	if self.is_inside_tree(): 
+		var is_instance = get_tree().get_edited_scene_root() != get_parent()
+		properties.append({
+			"name": "lua_path",
+			"type": TYPE_STRING,
+			"hint_string": "*.lua",
+			"hint": PROPERTY_HINT_FILE,
+			"usage": PROPERTY_USAGE_READ_ONLY if is_instance else PROPERTY_USAGE_DEFAULT
+		})
+	return properties
