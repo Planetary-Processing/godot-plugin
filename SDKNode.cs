ef89dbb77d9d25aaab5331a408f90fc50fb87e94
James Lovatt <james.lovatt@planetaryprocessing.io>
2023-12-22 15:43:07 +0000

    login works, print entities each tick

--- SDKNode.cs
+++ SDKNode.cs
@@ -6,21 +6,60 @@ using Planetary;
 public partial class SDKNode : Node
 {	
 	private SDK sdk;
+	private ulong gameId;
 	
-	public void Login(ulong gameId, string username, string password)
+	public void SetGameID(ulong gameId)
 	{
+		this.gameId = gameId;
 		sdk = new SDK(
-			gameId,
-			username,
-			password,
-			s => GD.Print(s)
+			gameId
 		);
-		GD.Print("sdk instantiated for game ID " + gameId);
+		GD.Print("SDKNode game ID " + gameId);
+	}
+	
+	public string Connect(string username, string password)
+	{
+		try 
+		{
+			sdk.Connect(username, password);
+			GD.Print("Player " + username + " authenticated on game ID " + gameId);
+			return "";
+		}
+		catch (Exception e)
+		{
+			return e.Message;
+		}
+	}
+	
+	public void Message(Dictionary<string, dynamic> msg)
+	{
+		sdk.Connect("Hello Message");
 	}
 
-	public override void _Process(double delta)
+	public void Update()
 	{
-		GD.Print("update");
 		sdk.Update();
+	}
+	
+	public Godot.Collections.Dictionary<string, Godot.Variant> GetEntities()
+	{
+		Godot.Collections.Dictionary<string, Godot.Variant> gdEntities = new Godot.Collections.Dictionary<string, Godot.Variant>();
+
+		foreach (var pair in sdk.entities)
+		{
+			Entity entity = pair.Value;
+			Godot.Collections.Dictionary<string, Godot.Variant> gdEntity = new Godot.Collections.Dictionary<string, Godot.Variant>();
+
+			gdEntity["id"] = entity.id;
+			gdEntity["x"] = entity.x;
+			gdEntity["y"] = entity.y;
+			gdEntity["z"] = entity.z;
+			gdEntity["data"] = entity.data;
+			gdEntity["type"] = entity.type;
+
+			gdEntities[entity.id] = gdEntity;
+		}
+
+		return gdEntities;
 	}
 }
