b8004f95649bfd9ee64b67ac64b5ea4d31a69584
Samuel Sully <sam@planetaryprocessing.io>
2024-09-25 12:38:21 +0100

    remove redundant features

--- pp_entity_node.gd
+++ pp_entity_node.gd
@@ -6,50 +6,13 @@ const Utils = preload("res://addons/planetary_processing/pp_utils.gd")
 signal state_changed(new_state)
 
 static var is_entity_node: bool = true
-
-func get_swarmplay_repo_directory() -> String:
-	if Engine.is_editor_hint():
-		var editor_settings = EditorInterface.get_editor_settings()
-		var swarmplay_repo_directory = editor_settings.get_setting("user/swarmplay_repo_directory")
-		if swarmplay_repo_directory.strip_edges() == "":
-			assert(false, "The SwarmPlay repo directory path is not set.")
-
-		var dir_access = DirAccess.open(swarmplay_repo_directory)
-		if dir_access == null:
-			assert(false, "The SwarmPlay repo directory does not exist: " + swarmplay_repo_directory)
-		return swarmplay_repo_directory
-	return ""
 	
 
 @export_category("Entity Properties")
-@export_multiline var data = ''
-@export var chunkloader = false
 var type = '' : set = _set_type, get = _get_type
-var lua_path = ''
 var entity_id = ''
 var pp_root_node
 var is_instance
-
-func _on_button_pressed(text:String):
-	assert(
-		type, "no type provided"
-	)
-	assert(
-		not FileAccess.file_exists(lua_path),
-		"lua file named " + type + ".lua already exists"
-	)
-	Utils.write_string_to_file(lua_path, "local function init(e)
-end
-
-local function update(e, dt)
-end
-
-local function message(e, msg)
-end
-
-return {init=init,update=update,message=message}
-")
-	Utils.refresh_filesystem()
 
 func _get_property_list() -> Array[Dictionary]:
 	var properties: Array[Dictionary] = []
@@ -58,45 +21,21 @@ func _get_property_list() -> Array[Dictionary]:
 		"type": TYPE_STRING,
 		"usage": PROPERTY_USAGE_READ_ONLY | PROPERTY_USAGE_EDITOR if is_instance else PROPERTY_USAGE_DEFAULT
 	})
-	if not is_instance:
-		properties.append({
-			"name": "pp_button_generate_lua_skeleton_file",
-			"type": TYPE_STRING,
-			"usage": PROPERTY_USAGE_DEFAULT
-		})
-		properties.append({
-			"name": "lua_path",
-			"type": TYPE_STRING,
-			"usage": PROPERTY_USAGE_READ_ONLY | PROPERTY_USAGE_EDITOR
-		})
 	return properties
 
 func _set_type(new_type: String):
-	var base_path = get_swarmplay_repo_directory()
 	type = new_type
-	if Engine.is_editor_hint() and is_inside_tree():
-		if not type:
-			lua_path = ''
-		else:
-			lua_path = base_path + "/entity/" + type + ".lua"
-			print("Lua path set to " + lua_path)
 
 func _get_type():
 	return type
 
 func _enter_tree():
-	var base_path = get_swarmplay_repo_directory()
 	is_instance = get_tree().get_edited_scene_root() != get_parent()
 	if Engine.is_editor_hint():
 		if not is_instance:
 			if not type:
 				# set the default type value based on the name
 				type = get_parent().name
-			if not lua_path:
-				# select existing lua file for lua path if exists
-				var filepath = base_path + '/entity/' + type + ".lua"
-				if FileAccess.file_exists(filepath):
-					lua_path = filepath
 		return
 	pp_root_node = get_tree().current_scene.get_node('PPRootNode')
 	assert(pp_root_node, "PPRootNode not present as direct child of parent scene")
--- pp_root_node.gd
+++ pp_root_node.gd
@@ -21,7 +21,7 @@ var game_id = ''
 #var password = ''
 var logged_in = false
 var registered_entities = []
-var swarmplay_repo_directory = '' : set = _set_swarmplay_repo_directory, get = _get_swarmplay_repo_directory
+
 
 var csproj_reference_exists = false
 var client = PPHTTPClient.new()
@@ -30,22 +30,6 @@ var player_uuid = null
 var timer: Timer
 var timer_wait_in_s = 10
 var sdk_node
-
-func _set_swarmplay_repo_directory(new_dir: String) -> void:
-	swarmplay_repo_directory = new_dir
-
-	if Engine.is_editor_hint():
-		var _editor_interface = Engine.get_singleton("EditorInterface")
-		var settings = _editor_interface.get_editor_settings()
-		settings.set_setting("user/swarmplay_repo_directory", new_dir)
-
-func _get_swarmplay_repo_directory() -> String:
-	if Engine.is_editor_hint():
-		var _editor_interface = Engine.get_singleton("EditorInterface")
-		var settings = _editor_interface.get_editor_settings()
-		var dir = settings.get_setting("user/swarmplay_repo_directory")
-		return dir if dir else ""
-	return swarmplay_repo_directory
 
 func _ready():
 	if Engine.is_editor_hint():
@@ -128,41 +112,6 @@ func _get_property_list() -> Array[Dictionary]:
 		"type": TYPE_STRING,
 		"usage": PROPERTY_USAGE_READ_ONLY | PROPERTY_USAGE_DEFAULT if logged_in else PROPERTY_USAGE_DEFAULT
 	})
-	#properties.append({
-		#"name": "username",
-		#"type": TYPE_STRING,
-		#"usage": PROPERTY_USAGE_READ_ONLY | PROPERTY_USAGE_DEFAULT if logged_in else PROPERTY_USAGE_DEFAULT
-	#})
-	#properties.append({
-		#"name": "password",
-		#"hint": PROPERTY_HINT_PASSWORD,
-		#"type": TYPE_STRING,
-		#"usage": PROPERTY_USAGE_READ_ONLY | PROPERTY_USAGE_SECRET | PROPERTY_USAGE_EDITOR if logged_in else PROPERTY_USAGE_SECRET | PROPERTY_USAGE_DEFAULT 
-	#})
-	properties.append({
-		"name": "swarmplay_repo_directory",
-		"type": TYPE_STRING,
-		"hint": PROPERTY_HINT_DIR,
-		"usage": PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_EDITOR,
-		"hint_string": ""
-	})
-	#if logged_in:
-		#properties.append({
-			#"name": "pp_button_logout",
-			#"type": TYPE_STRING,
-			#"usage": PROPERTY_USAGE_DEFAULT
-		#})
-	#else:
-		#properties.append({
-			#"name": "pp_button_login",
-			#"type": TYPE_STRING,
-			#"usage": PROPERTY_USAGE_DEFAULT
-		#})
-	properties.append({
-		"name": "pp_button_generate_init.json",
-		"type": TYPE_STRING,
-		"usage": PROPERTY_USAGE_DEFAULT
-	})
 	if not csproj_reference_exists:
 		properties.append({
 			"name": "pp_button_add_csproj_reference",
@@ -179,17 +128,6 @@ func _validate_fields():
 			"valid": false,
 			"message": "Game ID should be a numeric value"
 		}
-	#regex.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
-	#if not regex.search(username):
-		#return {
-			#"valid": false,
-			#"message": "Username should be a valid email address"
-		#}
-	#if password.length() < 6:
-		#return {
-			#"valid": false,
-			#"message": "Password must be at least 6 characters long"
-		#}
 	return {
 		"valid": true,
 		"message": ""
@@ -201,35 +139,7 @@ func _on_button_pressed(text:String):
 	if text.to_lower() == "add csproj reference":
 		_on_csproj_button_pressed()
 		return
-	if text.to_lower() == "generate init.json":
-		_on_generate_init_json_button_pressed()
-		return
-	#if text.to_lower() == "login":
-		#_on_login_button_pressed()
-		#return
-	#if text.to_lower() == "logout":
-		#_on_logout_button_pressed()
-		#return
-
-#func _on_login_button_pressed():
-	#var resp = client.post('/apis/httputils/HTTPUtils/WhoAmI', {}, username, password)
-	#if !resp:
-		#return
-	#var json = JSON.new()
-	#var result = json.parse(resp)
-	#var data = json.data
-	#assert("Approved" in data, "Malformed response")
-	#assert(data["Approved"], "User is not approved")
-	## check user has access to provided game
-	#resp = client.post('/apis/gamestore/GameStore/GetGame', { "GameID": game_id }, username, password)
-	#assert(resp, "User does not have access to this game")
-	#logged_in = true
-	#notify_property_list_changed()
-#
-#func _on_logout_button_pressed():
-	#logged_in = false
-	#notify_property_list_changed()
-
+		
 func _get_csharp_error_msg():
 	return "C# solution not created. Trigger Project > Tools > C# > Create C# Solution"
 
@@ -238,9 +148,6 @@ func _on_csproj_button_pressed():
 	assert(len(csproj_files), _get_csharp_error_msg())
 	Utils.add_planetary_csproj_ref('res://' + csproj_files[0])
 	notify_property_list_changed()
-
-func _on_generate_init_json_button_pressed():
-	_generate_init_json()
 
 func _get_entity_nodes():
 	var entity_nodes : Array = []
@@ -267,41 +174,6 @@ func _recursive_scene_entity_removal(node):
 			break
 		_recursive_scene_entity_removal(child)
 		
-func _generate_init_json():
-	if swarmplay_repo_directory.strip_edges() == "":
-		assert(false, "The SwarmPlay repo directory path is not set.")
-		return
-
-	var dir_access = DirAccess.open(swarmplay_repo_directory)
-	if dir_access == null:
-		assert(false, "The SwarmPlay repo directory does not exist: " + swarmplay_repo_directory)
-		return
-		
-	var entity_nodes = _get_entity_nodes()
-	
-	var entity_init_data = []
-	for entity_node in entity_nodes:
-		var scene_instance = entity_node.get_parent()
-		var data = {}
-		if entity_node.data:
-			var json = JSON.new()
-			var result = json.parse(entity_node.data)
-			assert(result == OK, 'invalid json found in data field of entity: ' + scene_instance.name)
-			data = json.data
-		entity_init_data.append({
-			'data': data,
-			'chunkloader': entity_node.chunkloader,
-			'type': entity_node.type,
-			'x': scene_instance.global_transform.origin.x,
-			'z': scene_instance.global_transform.origin.y,
-			'y': scene_instance.global_transform.origin.z
-		})
-		
-	var data = JSON.stringify(entity_init_data)
-	Utils.write_string_to_file(swarmplay_repo_directory + '/init.json', data)
-	Utils.refresh_filesystem()
-	print("init.json generated")
-	
 func _enter_tree():
 	if not Engine.is_editor_hint():
 		_remove_all_entity_scenes()
